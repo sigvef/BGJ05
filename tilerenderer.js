ac1b13659d08d1b0dd19e90ee90ce95f7c8cb4e3
Andreas Loeve Selvik <andreas.l.selvik@gmail.com>
2013-06-09 10:44:46 +0200

    tiny code edit in rendering code that is commented out

--- tilerenderer.js
+++ tilerenderer.js
@@ -34,11 +34,11 @@ TileRenderer.prototype.fits = function(arr){
 };
 
 TileRenderer.prototype.blit = function(sprite, srcX, srcY){
-//    this.context.drawImage(sprite, /*srcX, srcY, blockSize*GU, blockSize*GU,*/ 
-//            this.render_col*blockSize*GU, this.render_row*blockSize*GU, blockSize*GU, blockSize*GU);*/
     var blockSize = this.world.blockSize;
     this.context.drawImage(sprite, this.render_col*blockSize*GU, this.render_row*blockSize*GU, blockSize*GU, blockSize*GU);
-//    this.context.fillText(this.render_col+","+this.render_row,this.render_col*blockSize*GU, this.render_row*blockSize*GU, blockSize*GU, blockSize*GU);
+    //this.context.drawImage(sprite, srcX, srcY, blockSize*GU, blockSize*GU, 
+            //this.render_col*blockSize*GU, this.render_row*blockSize*GU, blockSize*GU, blockSize*GU);
+   // this.context.fillText(this.render_col+","+this.render_row,this.render_col*blockSize*GU, this.render_row*blockSize*GU, blockSize*GU, blockSize*GU);
 
 };
 
@@ -66,12 +66,12 @@ TileRenderer.prototype.render = function(render_row, render_col){
 }*/
 
 TileRenderer.prototype.renderWall = function(){
-    /*var idx = 0;
+    var idx = 0;
     if (this.isPath(-1,0))  idx += 1;
     if (this.isPath(0,1))   idx += 2;
     if (this.isPath(1,0))   idx += 4;
-    if (this.isPath(0,-1))  idx += 8;*/
-    this.blit(this.renderer.wall,0 ,0); // idx * 16);
+    if (this.isPath(0,-1))  idx += 8;
+    this.blit(this.renderer.wall,0 , idx * 16);
 };
 
 TileRenderer.prototype.renderRoad = function(){
