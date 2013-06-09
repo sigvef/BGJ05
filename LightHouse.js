17a6f5326c816044f1a7820208accb1b70652c1c
Andreas Loeve Selvik <andreas.l.selvik@gmail.com>
2013-06-09 13:16:46 +0200

    Actually added the LightHouse skeleton file

---
+++ LightHouse.js
@@ -0,0 +1,14 @@
+function LightHouse(x, y){
+    this.x = x;
+    this.y = y;
+    this.size = 4;
+}
+
+LightHouse.prototype.update = function(){
+
+
+}
+
+LightHouse.prototype.render = function(ctx, viewport){
+
+}
